9ef8019c34850ebdc2d794729ca65515e83e7f60
nicole-mathias <nicolemathias0@gmail.com>
2026-02-16 16:12:43 -0500

    main class

---
+++ src/main/java/com/library/LibraryService.java
@@ -0,0 +1,11 @@
+package com.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryService {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryService.class, args);
+    }
+}
